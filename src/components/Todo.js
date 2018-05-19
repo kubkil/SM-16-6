2f9c2c4dca17ab1dad088ffbe92a3b3b631f21d6
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-19 15:20:55 +0200

    refactor and add styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import style from '../../styles/Todo.css';
+import styles from '../../styles/Todo.css';
 
-const Todo = props =>
-  <li className={style.appToDoItem}>
-    {props.text}
-    <button onClick={() => props.remove(props.id)}>X</button>
+const Todo = ({ todo, remove }) => (
+  <li id={todo.id} className={styles.todo}>
+    {todo.text}
+    <button onClick={() => remove(todo.id)}>X</button>
   </li>
+);
 
 export default Todo;
